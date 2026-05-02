d7cc24cc3c191e4ad3487423cbb8f3a58f2c2c0d
Your Name <you@example.com>
2026-05-02 10:41:29 +0600

    add idempotent producer integration test and set kafka producer retries

---
+++ ProductMicroservice/src/test/java/com/example/productmicroservice/IdempotentProducerIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.example.productmicroservice;
+
+import com.example.core.events.ProductCreatedEvent;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Map;
+
+@SpringBootTest
+public class IdempotentProducerIntegrationTest {
+
+    @Autowired
+    KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate;
+
+    @MockitoBean
+    KafkaAdmin kafkaAdmin;
+
+    @Test
+    void testProducerConfig_whenIdempotenceEnabled_assertsIdempotentProperties() {
+        //Arrange
+        ProducerFactory<String, ProductCreatedEvent> producerFactory = kafkaTemplate.getProducerFactory();
+        //Act
+        Map<String, Object> config = producerFactory.getConfigurationProperties();
+       //Assert
+        Assertions.assertEquals("true", config.get(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG));
+        Assertions.assertTrue("all".equalsIgnoreCase((String) config.get(ProducerConfig.ACKS_CONFIG)));
+
+        if (config.containsKey(ProducerConfig.RETRIES_CONFIG)) {
+            Assertions.assertTrue(
+                    Integer.parseInt(config.get(ProducerConfig.RETRIES_CONFIG).toString()) > 0
+            );
+        }
+    }
+}
